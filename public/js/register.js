f83cfb9fb653c02de83c4e89dcf22756b8b613ec
Cristian Usuga <usugahiguitacristian@gmail.com>
2023-10-13 21:59:16 -0500

    Cambio en las validaciones del registro

--- public/js/register.js
+++ public/js/register.js
@@ -1,43 +1,100 @@
+function validateForm() {
+    // Deshabilita el botón de "Enviar" para evitar envíos múltiples
+    var submitButton = document.querySelector(".submit-button");
+    submitButton.disabled = true;
+
+    var errorMessages = document.getElementsByClassName("error-message");
+    for (var i = 0; i < errorMessages.length; i++) {
+        errorMessages[i].textContent = "";
+    }
+
+    validateCedula();
+    validateNombre('primerNombre');
+    validateApellido('primerApellido');
+    validateSApellido('segundoApellido');
+    validateCorreo();
+    validatePassword();
+    validateConfirmPassword();
+    validateCelular();
+    validateTelefono();
+    validateFechaNacimiento();
+
+    var errorMessages = document.getElementsByClassName("error-message");
+    for (var i = 0; i < errorMessages.length; i++) {
+        if (errorMessages[i].textContent !== "") {
+            // Habilita el botón de "Enviar" nuevamente si hay errores
+            submitButton.disabled = false;
+            return false;
+        }
+    }
+
+    var formData = new FormData(document.getElementById("form_usuarios"));
+
+    var userData = {
+        "id_usuario": formData.get("cedula"),
+        "nombre": formData.get("primerNombre"),
+        "primer_apellido": formData.get("primerApellido"),
+        "segundo_apellido": formData.get("segundoApellido"),
+        "correo": formData.get("correo"),
+        "contraseña": formData.get("password1"),
+        "fecha_nacimiento": formData.get("fechaNacimiento"),
+        "celular": formData.get("celular"),
+        "telefono": formData.get("telefono"),
+        "rol_de_usuario": formData.get("rol"),
+        "tipo_de_documento": formData.get("tipoDocumento"),
+        "estado_de_usuario": formData.get("estado"),
+        "sexo_de_usuario": formData.get("sexo")
+    };
+
+    registerUser(userData);
+}
+
+
+
 function validateCedula() {
     var cedulaInput = document.getElementById("cedula");
     var cedulaError = document.getElementById("cedulaError");
-    var cedulaPattern = /^[0-9]{6,10}$/;
+    var cedulaPattern = /^\d{7,10}$/;
 
     if (!cedulaPattern.test(cedulaInput.value)) {
         cedulaInput.style.borderColor = "red";
-        cedulaError.textContent = "La cédula debe contener máximo 10 dígitos.";
-        cedulaError.style.display = "inline"; // Mostrar el mensaje de error
+        cedulaError.textContent = "La cédula debe tener entre 7 y 10 dígitos numéricos.";
+        cedulaError.style.display = "inline";
     } else {
         cedulaInput.style.borderColor = "";
         cedulaError.textContent = "";
-        cedulaError.style.display = "none"; // Ocultar el mensaje de error
+        cedulaError.style.display = "none";
     }
 }
 
-// Función para validar el nombre (letras, espacios, tildes)
 function validateNombre(inputId) {
     var nombreInput = document.getElementById(inputId);
     var nombreError = document.getElementById(inputId + "Error");
-    var nombrePattern = /^[a-zA-ZñÑáÁéÉíÍóÓúÚ\s]+$/;
+    var nombreValue = nombreInput.value.trim();
 
-    if (!nombrePattern.test(nombreInput.value)) {
+    if (nombreValue.length < 1) {
         nombreInput.style.borderColor = "red";
-        nombreError.textContent = "El nombre solo puede contener letras, espacios y tildes.";
+        nombreError.textContent = "El nombre debe tener  más caracteres.";
+    } else if (nombreValue.length > 60) {
+        nombreInput.style.borderColor = "red";
+        nombreError.textContent = "El nombre no puede tener más de 60 caracteres.";
     } else {
         nombreInput.style.borderColor = "";
         nombreError.textContent = "";
     }
 }
 
-// Función para validar el apellido (letras, espacios, tildes)
 function validateApellido(inputId) {
     var apellidoInput = document.getElementById(inputId);
     var apellidoError = document.getElementById(inputId + "Error");
-    var apellidoPattern = /^[a-zA-ZñÑáÁéÉíÍóÓúÚ\s]+$/;
+    var apellidoValue = apellidoInput.value.trim();
 
-    if (!apellidoPattern.test(apellidoInput.value)) {
+    if (apellidoValue.length < 2) {
         apellidoInput.style.borderColor = "red";
-        apellidoError.textContent = "El apellido solo puede contener letras, espacios y tildes.";
+        apellidoError.textContent = "El apellido debe tener al menos 2 caracteres.";
+    } else if (apellidoValue.length > 30) {
+        apellidoInput.style.borderColor = "red";
+        apellidoError.textContent = "El apellido no puede tener más de 30 caracteres.";
     } else {
         apellidoInput.style.borderColor = "";
         apellidoError.textContent = "";
@@ -47,18 +104,19 @@ function validateApellido(inputId) {
 function validateSApellido(inputId) {
     var apellidoInput = document.getElementById(inputId);
     var apellidoError = document.getElementById(inputId + "Error");
-    var apellidoPattern = /^[a-zA-ZñÑáÁéÉíÍóÓúÚ\s]*$/;
+    var apellidoValue = apellidoInput.value.trim();
 
-    if (apellidoInput.value.trim() !== '' && !apellidoPattern.test(apellidoInput.value)) {
+    if (apellidoValue.length === 0) {
+        apellidoError.textContent = "";
+    } else if (apellidoValue.length > 30) {
         apellidoInput.style.borderColor = "red";
-        apellidoError.textContent = "El apellido solo puede contener letras, espacios y tildes.";
+        apellidoError.textContent = "El segundo apellido no puede tener más de 30 caracteres.";
     } else {
         apellidoInput.style.borderColor = "";
         apellidoError.textContent = "";
     }
 }
 
-// Función para validar el correo (expresión regular)
 function validateCorreo() {
     var correoInput = document.getElementById("correo");
     var correoError = document.getElementById("correoError");
@@ -73,7 +131,6 @@ function validateCorreo() {
     }
 }
 
-// Función para validar la contraseña (recomendaciones básicas)
 function validatePassword() {
     var passwordInput = document.getElementById("password1");
     var passwordError = document.getElementById("passwordError");
@@ -88,7 +145,6 @@ function validatePassword() {
     }
 }
 
-// Función para validar que la contraseña coincida con la repetición de la contraseña
 function validateConfirmPassword() {
     var passwordInput = document.getElementById("password1");
     var confirmPasswordInput = document.getElementById("confirmPassword");
@@ -103,113 +159,109 @@ function validateConfirmPassword() {
     }
 }
 
-// Función para validar el número de celular (solo números, máximo 11)
 function validateCelular() {
     var celularInput = document.getElementById("celular");
     var celularError = document.getElementById("celularError");
-    var celularPattern = /^[0-9]{1,11}$/;
+    var celularPattern = /^3\d{9}$/;
 
     if (!celularPattern.test(celularInput.value)) {
         celularInput.style.borderColor = "red";
-        celularError.textContent = "El número de celular debe contener solo números y máximo 11 dígitos.";
+        celularError.textContent = "El número de celular no válido.";
     } else {
         celularInput.style.borderColor = "";
         celularError.textContent = "";
     }
 }
 
-// Función para validar el número de teléfono (solo números, máximo 11)
 function validateTelefono() {
     var telefonoInput = document.getElementById("telefono");
     var telefonoError = document.getElementById("telefonoError");
-    var telefonoPattern = /^[0-9]{1,11}$/;
+    var telefonoPattern = /^60\d{8}$/;
 
-    if (!telefonoPattern.test(telefonoInput.value)) {
+    var telefonoValue = telefonoInput.value.trim(); // Eliminar espacios en blanco al principio y al final
+
+    if (telefonoValue.length < 1) {
+        telefonoInput.style.borderColor = "";
+        telefonoError.textContent = "";
+    } else if (!telefonoPattern.test(telefonoValue) || !/^\d+$/.test(telefonoValue) || telefonoValue.length !== 10) {
         telefonoInput.style.borderColor = "red";
-        telefonoError.textContent = "El número de teléfono debe contener solo números y máximo 11 dígitos.";
+        telefonoError.textContent = "El número de teléfono no válido. Código de área + número de teléfono fijo.";
     } else {
         telefonoInput.style.borderColor = "";
         telefonoError.textContent = "";
     }
 }
 
-// Función para validar la fecha de nacimiento (mayor a 13 años)
+
+
+
+
 function validateFechaNacimiento() {
     var fechaNacimientoInput = document.getElementById("fechaNacimiento");
     var fechaNacimientoError = document.getElementById("fechaNacimientoError");
     var fechaNacimiento = new Date(fechaNacimientoInput.value);
     var hoy = new Date();
-    var edadMinima = new Date(hoy.getFullYear() - 13, hoy.getMonth(), hoy.getDate());
+    var edadMinima = new Date(hoy.getFullYear() - 14, hoy.getMonth(), hoy.getDate());
 
-    if (fechaNacimiento >= edadMinima) {
+    if (fechaNacimiento >= edadMinima || fechaNacimiento >= hoy) {
         fechaNacimientoInput.style.borderColor = "red";
-        fechaNacimientoError.textContent = "Debe tener al menos 13 años para registrarse.";
+        fechaNacimientoError.textContent = "Debe tener al menos 14 años.";
     } else {
         fechaNacimientoInput.style.borderColor = "";
         fechaNacimientoError.textContent = "";
     }
 }
 
-// Función para validar el formulario antes de enviarlo al servidor
-function validateForm() {
-    validateCedula();
-    validateNombre('primerNombre');
-    validateApellido('primerApellido');
-    validateSApellido('segundoApellido');
-    validateCorreo();
-    validatePassword();
-    validateConfirmPassword();
-    validateCelular();
-    validateTelefono();
-    validateFechaNacimiento();
+async function registerUser(userData) {
+    try {
+        const response = await fetch('http://localhost:8080/api/usuarios/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(userData)
+        });
 
-    // Verificar si hay algún error en los campos
-    var errorMessages = document.getElementsByClassName("error-message");
-    for (var i = 0; i < errorMessages.length; i++) {
-        if (errorMessages[i].textContent !== "") {
-            return false; // Hay errores, no se envía el formulario
-        }
-    }
-
-    // Si no hay errores, prepara el objeto de datos
-    var formData = {
-        nombre: document.getElementById("primerNombre").value,
-        primer_apellido: document.getElementById("primerApellido").value,
-        segundo_apellido: document.getElementById("segundoApellido").value,
-        correo: document.getElementById("correo").value,
-        contraseña: document.getElementById("password1").value,
-        fecha_nacimiento: document.getElementById("fechaNacimiento").value,
-        celular: document.getElementById("celular").value,
-        telefono: document.getElementById("telefono").value,
-        rol_de_usuario: document.getElementById("rol").value,
-        tipo_de_documento: document.getElementById("tipoDocumento").value,
-        estado_de_usuario: document.getElementById("estado").value,
-        sexo_de_usuario: document.getElementById("sexo").value
-    };
-
-    // Realiza una solicitud POST al API
-    fetch('/api/usuarios/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(formData)
-    })
-    .then(response => {
         if (response.ok) {
-            // Maneja un registro exitoso (por ejemplo, muestra un mensaje de éxito)
-            alert('¡Registro exitoso!');
-            // Opcionalmente, puedes redirigir al usuario a una página de inicio de sesión
-            window.location.href = '/login';
+            const responseData = await response.json();
+            Swal.fire({
+                icon: 'success',
+                title: 'Registro exitoso',
+                text: '¡Registro exitoso!',
+            }).then(() => {
+                window.location.href = '/login';
+            });
         } else {
-            // Maneja los errores del API (por ejemplo, muestra un mensaje de error)
-            alert('El registro falló. Por favor, inténtalo de nuevo más tarde.');
+            const errorMessage = await response.text();
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: errorMessage,
+            });
         }
-    })
-    .catch(error => {
-        // Maneja errores de red u otros errores
+    } catch (error) {
         console.error('Error:', error);
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'Ha ocurrido un error. Por favor, inténtalo de nuevo más tarde.',
+        });
+    } finally {
+        // Habilita el botón de "Enviar" nuevamente después de manejar la respuesta
+        var submitButton = document.querySelector(".submit-button");
+        submitButton.disabled = false;
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("form_usuarios").addEventListener("submit", function (event) {
+        event.preventDefault();
+        //validateForm();
     });
 
-    return false; // Evita que el formulario se envíe mediante la presentación HTML de formulario estándar
-}
+    var maxDate = new Date();
+    maxDate.setFullYear(maxDate.getFullYear() - 14);
+    var formattedMaxDate = maxDate.toISOString().split('T')[0];
+    document.getElementById("fechaNacimiento").setAttribute("max", formattedMaxDate);
+
+});
